349ce300bd3bd8c571bf09aead343ee827c55858
Urooj-Iftikhar <uroojftkhr@gmail.com>
2026-02-18 22:53:56 +0500

    Update student_code.cpp

--- src/student_code.cpp
+++ src/student_code.cpp
@@ -10,16 +10,22 @@ int getIntValue() {
 float getFloatValue() {
     // Declare a variable var_float of type float and set its value to 10.75.
     //return var_float;
+    float var_float=10.75;
+    return var_float;
+    
 }
 
 char getCharValue() {
     // Declare a variable var_char of type character and set its value to A.
     //return var_char;
+    char var_char='A';
+    return var_char;
+    
 }
 
 int convertFloatToInt(float value) {
     // convert value to int.
-    return value;
+    return (int)value;
 }
 
 /*
@@ -28,6 +34,7 @@ Convert Fahrenheit to Celsius and store your answer in a variable celsius.
 Formula:
     Celsius = (Fahrenheit - 32) * 5 / 9
 */
+ Celsius = (Fahrenheit - 32) * 5 / 9
 float fahrenheitToCelsius(float fahrenheit) {
     //return celsius;
 
